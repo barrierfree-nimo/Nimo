7ddfe01fad9bd2cc2218a5a7b7caa9b2c78500cd
songah <skutcm119@gmail.com>
2022-11-20 08:51:37 +0900

    [chore]확인용 코드

--- RN/App.tsx
+++ RN/App.tsx
@@ -4,6 +4,10 @@ import { SafeAreaView, StyleSheet, Text } from "react-native";
 import Main from "./src/components/main/main";
 import Login from "./src/components/login/login";
 import SimulMain from "./src/components/simul_main/simul_main";
+import MessageSimul from "./src/components/simul_message/simul_message";
+import MessageDetail from "./src/components/simul_message/[id]";
+import CorrectPage from "./src/components/simul_message/correct";
+import WrongPage from "./src/components/simul_message/wrong";
 
 const App = () => {
   return (
@@ -11,6 +15,10 @@ const App = () => {
       {/* <Login /> */}
       {/* <Main /> */}
       <SimulMain />
+      {/* <MessageSimul /> */}
+      {/* <MessageDetail /> */}
+      {/* <CorrectPage /> */}
+      {/* <WrongPage /> */}
     </SafeAreaView>
   );
 };
