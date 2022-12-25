1db9bbb9cbbba30de692020ec4bfd8263b044dda
suhhyun <2835223@ewhain.net>
2022-12-25 23:41:56 +0900

    Add settings

--- node/api/routes/user.js
+++ node/api/routes/user.js
@@ -2,8 +2,9 @@ const {application} = require("express");
 const express = require("express");
 const router = express.Router();
 const userController = require('../../controllers/user');
+const auth = require('../middlewares/auth').checkTokens
 
 router.post("/join", userController.createNewUser);
 router.post("/login", userController.createToken);
-
+router.post("/info", auth, userController.setInfo);
 module.exports = router;
