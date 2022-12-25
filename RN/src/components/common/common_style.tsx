9fde5ecfa18a8e72044907811d9711a9eb73200a
songah <skutcm119@gmail.com>
2022-12-26 04:25:43 +0900

    refactor: 체험 나가기 버튼 컴포넌트 스타일링 추가

--- RN/src/components/common/common_style.tsx
+++ RN/src/components/common/common_style.tsx
@@ -1,20 +1,21 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const SCREEN_WIDTH = Dimensions.get("window").width
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const CommonStyle = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#00284E'
-  },  
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#00284E",
+  },
   container_header: {
     width: SCREEN_WIDTH,
     height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'orange'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
   },
   container_contents: {
     width: "95%",
@@ -25,21 +26,41 @@ const CommonStyle = StyleSheet.create({
     width: SCREEN_WIDTH - 80,
     height: 50,
     marginBottom: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 20,
-    backgroundColor: 'coral'
+    backgroundColor: "coral",
   },
   btnText_exit: {
     fontSize: 25,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   title: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     fontSize: 30,
-    fontWeight: 'bold',
-  }
-})
+    fontWeight: "bold",
+  },
+  exit_div: {
+    position: "absolute",
+    top: SCREEN_HEIGHT - 60,
+    width: SCREEN_WIDTH - 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  exit_btn: {
+    width: SCREEN_WIDTH - 150,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FF4D4D",
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  exit_btn_text: {
+    fontSize: 30,
+    fontWeight: "800",
+  },
+});
 
 export default CommonStyle;
