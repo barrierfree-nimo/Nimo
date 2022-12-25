a17a4042ea79d2b7879959502395a315ec35ef1d
suhhyun <2835223@ewhain.net>
2022-12-25 23:52:54 +0900

    Add auth

--- node/api/routes/settings.js
+++ node/api/routes/settings.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const settingController = require('../../controllers/settings');
 const auth = require('../middlewares/auth').checkTokens
 
-const { json, HasMany } = require("sequelize");
-
 router.get("/", auth, settingController.userInfo);
 router.post("/password", auth, settingController.resetPassword);
 router.post("/info", auth, settingController.setInfo);
