6e35146718c81b95f2d376a5b82cd4d6807c6f42
suhhyun <2835223@ewhain.net>
2022-12-25 23:41:34 +0900

    Add settings page

---
+++ node/controllers/settings.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const {User, History, Msg, Sns, SimulData} = require("../models");
+var Sequelize = require("sequelize");
+const {json, HasMany} = require("sequelize");
+
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const jwt = require('jsonwebtoken');
+
+const settings = {
+    userInfo: async function (req, res, next) {
+        try {
+            const user = await User.findOne({where: {id: req.user_id}});
+            //console.log(user)
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(400)
+        }
+    },
+    resetPassword: async function (req, res, next) {
+        try {
+            //사용자 검증 질문 추가
+            const user = await User.findOne({where: {id: req.user_id}});
+            const password = req.body.password
+            user.update({password: password})
+            res.status(200).json("msg: successfully update pw")
+        } catch (error) {
+            res.status(401)
+        }
+    },
+    setInfo: async function (req, res, next) {
+        try {
+            const user = await User.findOne({where: {id: req.user_id}});
+            const {birth, gender, job, interest, offspring, bank} = req.body;
+            try {
+                user.update({
+                    birth: birth,
+                    gender: gender,
+                    job: job,
+                    interest: interest,
+                    offspring: offspring,
+                    bank: bank
+                })
+            } catch (error) {
+                console.log(error)
+                res.status(401)
+            }
+            res.status(200).json("msg: success update userinfo")
+        } catch (error) {
+            res.status(400)
+        }
+    }
+}
+module.exports = settings;
