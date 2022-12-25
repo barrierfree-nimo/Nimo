3ade679b654058c5dfb2d44e9b4ed6a1031b638e
songah <skutcm119@gmail.com>
2022-12-26 04:29:35 +0900

    feat: simulation main 페이지 서버연결 및 변경사항 반영

--- RN/src/components/simul_main/simul_main.tsx
+++ RN/src/components/simul_main/simul_main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
   TouchableOpacity,
@@ -7,13 +7,44 @@ import {
   Text,
 } from "react-native";
 import CommonStyle from "../common/common_style";
+import ExitBtn from "../simul_common/exit_btn";
 import SimulMainStyle from "./simul_main_style";
+import Axios from "axios";
+import baseURL from "../baseURL";
 
 const SimulMain = ({ navigation }: any) => {
-  const [push, setPush] = useState<string>("message");
-  const [messageType, setMessageType] = useState<string>("red");
-  const [snsType, setSnsType] = useState<string>("red");
-  const [callType, setCallType] = useState<string>("red");
+  const [type, setType] = useState<string>("");
+  const [num, setNum] = useState<number>();
+  const [red, setRed] = useState<string[]>();
+
+  useEffect(() => {
+    fetchSimulMain();
+  }, []);
+
+  const fetchSimulMain = async () => {
+    const token = ``;
+    try {
+      Axios.get(baseURL + "/simulation", {
+        headers: {
+          accessToken: `${token}`,
+        },
+      }).then((res) => {
+        setTimeout(() => setType(res.data.type), 2000);
+        setNum(res.data.num);
+        setRed(res.data.red);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    console.log(type, num, red);
+  }, [red]);
+
+  const isInRed = (appType: string) => {
+    if (red?.includes(appType)) return true;
+  };
 
   return (
     <SafeAreaView style={CommonStyle.container}>
@@ -25,7 +56,7 @@ const SimulMain = ({ navigation }: any) => {
       <View style={SimulMainStyle.phone_div}>
         {/* 앱 푸시 바 */}
         <View style={SimulMainStyle.phone_push_div}>
-          {push === "sns" && (
+          {type === "sns" && (
             <TouchableOpacity
               onPress={() => navigation.navigate("MessageSimul")}
             >
@@ -35,7 +66,7 @@ const SimulMain = ({ navigation }: any) => {
               />
             </TouchableOpacity>
           )}
-          {push === "message" && (
+          {type === "message" && (
             <TouchableOpacity
               onPress={() => navigation.navigate("MessageSimul")}
             >
@@ -45,7 +76,7 @@ const SimulMain = ({ navigation }: any) => {
               />
             </TouchableOpacity>
           )}
-          {push === "call" && (
+          {type === "call" && (
             <TouchableOpacity>
               <Image
                 source={require("../../assets/icons/simul_main/call_push.png")}
@@ -54,6 +85,7 @@ const SimulMain = ({ navigation }: any) => {
             </TouchableOpacity>
           )}
         </View>
+
         {/*  앱 바탕 */}
         <View style={SimulMainStyle.phone_app_div}>
           <>
@@ -102,21 +134,15 @@ const SimulMain = ({ navigation }: any) => {
               style={SimulMainStyle.img_app_icon}
             />
           </>
+
           {/*  SNS */}
-          <TouchableOpacity>
-            {push === "sns" && snsType === "star" && (
-              <Image
-                source={require(`../../assets/icons/simul_main/sns_star.png`)}
-                style={SimulMainStyle.img_app_icon}
-              />
-            )}
-            {push === "sns" && snsType === "red" && (
+          <TouchableOpacity onPress={() => navigation.navigate("MessageSimul")}>
+            {type === "sns" || isInRed("sns") ? (
               <Image
                 source={require(`../../assets/icons/simul_main/sns_red.png`)}
                 style={SimulMainStyle.img_app_icon}
               />
-            )}
-            {push !== "sns" && (
+            ) : (
               <Image
                 source={require(`../../assets/icons/simul_main/sns_default.png`)}
                 style={SimulMainStyle.img_app_icon}
@@ -124,23 +150,17 @@ const SimulMain = ({ navigation }: any) => {
             )}
           </TouchableOpacity>
         </View>
+
         {/*  앱 하단바 */}
         <View style={SimulMainStyle.phone_bottom_div}>
           {/*  MSG */}
           <TouchableOpacity onPress={() => navigation.navigate("MessageSimul")}>
-            {push === "message" && messageType === "star" && (
-              <Image
-                source={require(`../../assets/icons/simul_main/message_star.png`)}
-                style={SimulMainStyle.img_bottom_app_icon}
-              />
-            )}
-            {push === "message" && messageType === "red" && (
+            {type === "message" || isInRed("message") ? (
               <Image
                 source={require(`../../assets/icons/simul_main/message_red.png`)}
                 style={SimulMainStyle.img_bottom_app_icon}
               />
-            )}
-            {push !== "message" && (
+            ) : (
               <Image
                 source={require("../../assets/icons/simul_main/message_default.png")}
                 style={SimulMainStyle.img_bottom_app_icon}
@@ -157,20 +177,17 @@ const SimulMain = ({ navigation }: any) => {
           </TouchableOpacity>
 
           {/*  CALL */}
-          <TouchableOpacity onPress={() => {navigation.navigate("VoiceSimulMain")}}>
-            {push === "call" && callType === "star" && (
-              <Image
-                source={require(`../../assets/icons/simul_main/call_star.png`)}
-                style={SimulMainStyle.img_bottom_app_icon}
-              />
-            )}
-            {push === "call" && callType === "red" && (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("VoiceSimulMain");
+            }}
+          >
+            {type === "call" || isInRed("call") ? (
               <Image
                 source={require(`../../assets/icons/simul_main/call_red.png`)}
                 style={SimulMainStyle.img_bottom_app_icon}
               />
-            )}
-            {push !== "call" && (
+            ) : (
               <Image
                 source={require("../../assets/icons/simul_main/call_default.png")}
                 style={SimulMainStyle.img_bottom_app_icon}
@@ -181,13 +198,7 @@ const SimulMain = ({ navigation }: any) => {
       </View>
 
       {/* 이동버튼 */}
-      <View style={SimulMainStyle.exit_div}>
-        <TouchableOpacity onPress={() => navigation.navigate("Main")}>
-          <View style={SimulMainStyle.exit_btn}>
-            <Text style={SimulMainStyle.exit_btn_text}>체험 나가기</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <ExitBtn navigation={navigation} />
     </SafeAreaView>
   );
 };
