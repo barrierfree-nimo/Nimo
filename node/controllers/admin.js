62ae8f047b0bd91083427d3ed2577aeb7557e37a
suhhyun <2835223@ewhain.net>
2023-01-10 07:08:50 +0900

    [feat] 차단 기능 완성

--- node/controllers/admin.js
+++ node/controllers/admin.js
@@ -4,40 +4,35 @@ var Sequelize = require("sequelize");
 const {json, HasMany} = require("sequelize");
 
 const settings = {
-    blockContents: async function (req, res, next) {
-        try {
-            const user = await User.findOne({where: {id: req.user_id}});
-            try {
-                const {type, id} = req.params
-                if(type=="user"){
-                    const block = await BlackUser.create({
-                        user_nickname: user.nickname,
-                        type: type,
-                        block_id: id
-                    })
-                } else if(type=="post"){
-                    const block = await BlackPost.create({
-                        user_nickname: user.nickname,
-                        type: type,
-                        block_id: id
-                    })
-                } else {
-                    const block = await BlackComment.create({
-                        user_nickname: user.nickname,
-                        type: type,
-                        block_id: id
-                    })
-                }
-                
-                console.log(block)
-            } catch(error){
-                return res.status(404).json(error)
-            }
-            return res.status(204).send()
-        } catch (error) {
-            return res.status(400)
-        }
-    }
+	blockContents: async function (req, res, next) {
+		try {
+			const user = await User.findOne({where: {id: req.user_id}});
+			try {
+				const {type, id} = req.params
+				if (type == "user") {
+					const block = await BlackUser.create({
+						user_nickname: user.nickname,
+						black_nickname: req.params.value
+					})
+				} else if (type == "post") {
+					const block = await BlackPost.create({
+						user_nickname: user.nickname,
+						black_id: req.params.value
+					})
+				} else {
+					const block = await BlackComment.create({
+						user_nickname: user.nickname,
+						black_id: req.params.value
+					})
+				}
+			} catch (error) {
+				return res.status(404).json(error)
+			}
+			return res.status(204).send()
+		} catch (error) {
+			return res.status(400)
+		}
+	}
 }
 
 module.exports = settings;
