efb160a156cef3587ebc2eaa5c49ae33f05f5a2a
suhhyun <2835223@ewhain.net>
2023-01-09 23:54:49 +0900

    [hotfix] change alarm logic

--- node/controllers/simulation.js
+++ node/controllers/simulation.js
@@ -9,60 +9,74 @@ const simulation = {
     randomApp: async function (req, res, next) {
         try {
             const user = await User.findOne({where: {id: req.user_id}});
-            arr = [ "sns", "msg", "voice" ];
-            //type = arr[Math.floor(Math.random() * 1)];
-            type = arr[ Math.round(Math.random()) ] // 0 or 1 (except voice)
-            console.log(type)
-            const doneNum = await History.findAll({
-                where: {
-                    user_nickname: user.nickname,
-                    type: type,
-                },
-                attributes: [ "simulNum" ],
-                raw: true,
-            });
-            const done = doneNum.map((x) => Number(x.simulNum));
-
-            if (type == "voice") {
-                var notDone = await Voice.findAll({
-                    limit: 1,
-                    where: {
-                        num: {[ Sequelize.Op.notIn ]: done},
-                    },
-                    attributes: [ "num" ],
-                    raw: true,
-                });
-                var num = notDone[ 0 ].num;
-            } else if (type == "msg") {
-                var notDone = await Msg.findAll({
-                    limit: 1,
-                    where: {
-                        num: {[ Sequelize.Op.notIn ]: done},
-                    },
-                    attributes: [ "num" ],
-                    raw: true,
-                });
-                var num = notDone[ 0 ].num;
-            } else if (type == "sns") {
-                var notDone = await Sns.findAll({
-                    limit: 1,
-                    where: {
-                        num: {[ Sequelize.Op.notIn ]: done},
-                    },
-                    attributes: [ "num" ],
-                    raw: true,
-                });
-                var num = notDone[ 0 ].num;
-            }
-
+            var arr = [ "sns", "msg", "voice" ];
+            let check = [];
             const red = []
-            for (t of [ "msg", "sns", "voice" ]) {
-                var calc = await SimulData.count({where: {type: t}}) - await History.count({where: {type: t, user_nickname: user.nickname}})
+            for (t of [ "sns", "msg", "voice" ]) {
+                const calc = await SimulData.count({where: {type: t}}) - await History.count({where: {type: t, user_nickname: user.nickname}})
                 if (calc > 0) {
                     red.push(t)
                 }
             }
 
+            while (true) {
+                var rand = Math.floor(Math.random() * 3)
+                var type = arr[ rand ];
+
+                const doneNum = await History.findAll({
+                    where: {
+                        user_nickname: user.nickname,
+                        type: type,
+                    },
+                    attributes: [ "simulNum" ],
+                    raw: true,
+                });
+
+                const done = doneNum.map((x) => Number(x.simulNum));
+                console.log(done)
+                if (type == "voice") {
+                    var notDone = await Voice.findAll({
+                        limit: 1,
+                        where: {
+                            num: {[ Sequelize.Op.notIn ]: done},
+                        },
+                        attributes: [ "num" ],
+                        raw: true,
+                    });
+                } else if (type == "msg") {
+                    var notDone = await Msg.findAll({
+                        limit: 1,
+                        where: {
+                            num: {[ Sequelize.Op.notIn ]: done},
+                        },
+                        attributes: [ "num" ],
+                        raw: true,
+                    });
+                } else if (type == "sns") {
+                    var notDone = await Sns.findAll({
+                        limit: 1,
+                        where: {
+                            num: {[ Sequelize.Op.notIn ]: done},
+                        },
+                        attributes: [ "num" ],
+                        raw: true,
+                    });
+                }
+
+                if (notDone.length !== 0) {
+                    console.log("not zero")
+                    var num = notDone[ 0 ].num;
+                    break;
+                } else {
+                    console.log("not")
+                    console.log(type)
+                    check[ rand ] = true
+                }
+
+                if (check[ 0 ] && check[ 1 ] && check[ 2 ]) {
+                    return res.status(200).json({red})
+                }
+            }
             return res.status(200).json({type, num, red});
         } catch (error) {
             return res.status(400)
