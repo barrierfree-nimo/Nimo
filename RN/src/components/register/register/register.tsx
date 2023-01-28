64fe77e4272acd23e80e7cdfb6d3c1af96cb539c
seoyamin <seoyamin@ewhain.net>
2023-01-29 01:34:21 +0900

    feat : 회원가입 (AsyncStorage yet)

--- RN/src/components/register/register/register.tsx
+++ RN/src/components/register/register/register.tsx
@@ -20,25 +20,43 @@ import RegisterStyle from "./register_style";
 import baseURL from "../../baseURL";
 
 const Register = ({ navigation }: any) => {
-  const [id, setId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [userIdOk, setUserIdOk] = useState(false);
   const [nickname, setNickname] = useState("");
+  const [nicknameOk, setNicknameOk] = useState(false);
   const [password, setPassword] = useState("");
   const [passwordAgain, setPasswordAgain] = useState("");
-  const [passwordAnswer, setPasswordAnswer] = useState("");
-  //const [idOk, setIdOk] = useState(false);
-  const [nicknameOk, setNicknameOk] = useState(false);
   const [passwordOk, setPasswordOk] = useState(false);
-  //const [passwordAnswerOk, setPasswordAnswerOk] = useState(false);
   const [terms1Ok, setTerms1Ok] = useState(false);
   const [terms2Ok, setTerms2Ok] = useState(false);
   const terms1URL = "https://sugary-cuticle-b44.notion.site/ef5867bd9c6a4696bb6b3eecca1bc266";
   const terms2URL = "https://sugary-cuticle-b44.notion.site/e8163ea69f81486aa5f8f1fa8857cb27";
 
 
+  const checkUserId = async () => {
+    try {
+      if (userId.length < 5) {
+        showToast("5자 이상의 아이디를 설정해주세요.");
+        return;
+      }
+
+      await Axios.get(baseURL + `/user/userid/${userId}`).then((res) => {
+        if (res.status == 200) setUserIdOk(true);
+        else {
+          setUserIdOk(false);
+          showToast("이미 존재하는 아이디입니다.");
+        }
+      });
+    } catch (err) {
+      setNicknameOk(false);
+      showToast("이미 존재하는 아이디입니다.");
+    }
+  };
+
   const checkNickname = async () => {
     try {
-      if (nickname.length < 5) {
-        showToast("5자 이상의 아이디를 설정해주세요.");
+      if (nickname.length < 2) {
+        showToast("3자 이상의 닉네임을 설정해주세요.");
         return;
       }
 
@@ -46,12 +64,12 @@ const Register = ({ navigation }: any) => {
         if (res.status == 200) setNicknameOk(true);
         else {
           setNicknameOk(false);
-          showToast("이미 존재하는 아이디입니다.");
+          showToast("이미 존재하는 닉네임입니다.");
         }
       });
     } catch (err) {
       setNicknameOk(false);
-      showToast("이미 존재하는 아이디입니다.");
+      showToast("이미 존재하는 닉네임입니다.");
     }
   };
 
@@ -65,32 +83,51 @@ const Register = ({ navigation }: any) => {
   };
 
   const addUser = () => {
-    navigation.navigate("RegisterCheck");
+    if(userId == "") {
+      showToast("아이디를 입력해주세요");
+      return;
+    }
 
-    // if (password.length < 8) {
-    //   showToast("8자 이상의 비밀번호를 설정해주세요.");
-    //   return;
-    // }
+    if(nickname == "") {
+      showToast("닉네임을 입력해주세요");
+      return;
+    }
 
-    // if(nickname != null )
+    if (password.length < 8) {
+      showToast("8자 이상의 비밀번호를 설정해주세요.");
+      return;
+    }
 
-    // if (nicknameOk && passwordOk && termsOk) {
-    //   Axios.post(baseURL + "/user/join", {
-    //     nickname: nickname,
-    //     password: password,
-    //   })
-    //     .then((res) => {
-    //       showToast("회원가입이 완료되었습니다.");
-    //       navigation.navigate("Login");
-    //     })
-    //     .catch((err) => console.log(err));
-    // } else if (!nicknameOk) {
-    //   showToast("아이디 중복 여부를 확인해주세요.");
-    // } else if (!passwordOk) {
-    //   showToast("비밀번호를 다시 확인해주세요");
-    // } else {
-    //   showToast("이용약관에 동의하셔야 가입이 가능합니다.");
-    // }
+    if (!passwordOk) {
+      showToast("비밀번호를 다시 확인해주세요");
+      return;
+    }
+
+    if(!(terms1Ok && terms2Ok)) {
+      showToast("모든 약관에 동의하셔야 가입이 가능합니다.");
+      return;
+    }
+
+    if (userIdOk && nicknameOk && passwordOk && terms1Ok && terms2Ok) {
+      Axios.post(baseURL + "/user/join", {
+        user_id: userId,
+        nickname: nickname,
+        password: password,
+      })
+        .then((res) => {
+          showToast("회원가입이 완료되었습니다.");
+          navigation.navigate("RegisterCheck");
+        })
+        .catch((err) => console.log(err));
+    } else if (!userId) {
+      showToast("아이디 중복 여부를 확인해주세요.");
+    } else if (!nicknameOk) {
+      showToast("닉네임 중복 여부를 확인해주세요.");
+    } else if (!passwordOk) {
+      showToast("비밀번호를 다시 확인해주세요");
+    } else {
+      showToast("이용약관에 동의하셔야 가입이 가능합니다.");
+    }
   };
 
   return (
@@ -107,23 +144,22 @@ const Register = ({ navigation }: any) => {
               <Text style={RegisterStyle.title_item}>아이디</Text>
               <View style={RegisterStyle.container_item_input}>
                 <TextInput
-                  // value={id}
-                  // onChangeText={(id) => {
-                  //   setId(id), setIdOk(false);
-                  // }}
+                  value={userId}
+                  onChangeText={(userId) => {
+                    setUserId(userId), setUserIdOk(false);
+                  }}
                   style={RegisterStyle.textInput_item_id}
                   placeholder="아이디"
                 />
                 <TouchableOpacity
                   style={RegisterStyle.btn_overlapping}
-                  onPress={() => checkNickname()}
-                >
+                  onPress={() => checkUserId()}>
                   <Text style={RegisterStyle.btnText_overlapping}>
                     중복확인
                   </Text>
                 </TouchableOpacity>
               </View>
-              {nicknameOk === false ? (
+              {userIdOk === false ? (
                 <View></View>
               ) : (
                 <Text style={RegisterStyle.text_notice}>
@@ -138,13 +174,26 @@ const Register = ({ navigation }: any) => {
                 <TextInput
                   value={nickname}
                   onChangeText={(nickname) => {
-                    setNickname(nickname), 
-                    setNicknameOk(false);
+                    setNickname(nickname), setNicknameOk(false);
                   }}
-                  style={RegisterStyle.textInput_item}
+                  style={RegisterStyle.textInput_item_id}
                   placeholder="닉네임"
                 />
+                <TouchableOpacity
+                  style={RegisterStyle.btn_overlapping}
+                  onPress={() => checkNickname()}>
+                  <Text style={RegisterStyle.btnText_overlapping}>
+                    중복확인
+                  </Text>
+                </TouchableOpacity>
               </View>
+              {nicknameOk === false ? (
+                <View></View>
+              ) : (
+                <Text style={RegisterStyle.text_notice}>
+                  * 사용 가능한 닉네임입니다.
+                </Text>
+              )}
             </View>
 
             <View style={RegisterStyle.container_item}>
@@ -181,21 +230,6 @@ const Register = ({ navigation }: any) => {
                   * 비밀번호 확인이 완료되었습니다.
                 </Text>
               )}
-            </View>
-
-            <View style={RegisterStyle.container_item}>
-              <Text style={RegisterStyle.title_item}>비밀번호 찾기 질문</Text>
-              <View style={RegisterStyle.container_item_input}>
-                <TextInput
-                  // value={passwordAnswer}
-                  // onChangeText={(passwordAnswer) => {
-                  //   setPasswordAnswer(passwordAnswer),
-                  //   setPasswordAnswerOk(false)
-                  // }}
-                  style={RegisterStyle.textInput_item}
-                  placeholder="가장 아끼는 보물 1호는?"
-                />
-              </View>
             </View>
 
             <View style={RegisterStyle.container_item}>
